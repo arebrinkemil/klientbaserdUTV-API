f8c90ad3a7dafbb7471f94da8b8e575b2da679c1
arebrinkemil <60137005+arebrinkemil@users.noreply.github.com>
2023-11-24 17:37:09 +0100

    update

--- script.js
+++ script.js
@@ -1,19 +1,66 @@
 const API_KEY = "AIzaSyBGoaAhnmGXO2mLUevU1VWGo8FGFiTjabQ ";
 
-function search() {
-  var x = document.getElementById("query").value;
-  searchTerm = x;
+function searchChannel() {
+  var channelName = document.getElementById("query").value;
 
   fetch(
-    `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${searchTerm}&key=${API_KEY}`
+    `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${channelName}&type=channel&key=${API_KEY}`
   )
     .then((response) => response.json())
     .then((data) => {
-      displayVideos(data.items);
+      if (data.items.length > 0) {
+        const channelId = data.items[0].id.channelId;
+        getChannelUploadsPlaylistId(channelId);
+      } else {
+        console.log("Channel not found");
+      }
     })
     .catch((error) => {
       console.log(error);
     });
+}
+
+function getChannelUploadsPlaylistId(channelId) {
+  fetch(
+    `https://www.googleapis.com/youtube/v3/channels?part=contentDetails&id=${channelId}&key=${API_KEY}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      const uploadsPlaylistId =
+        data.items[0].contentDetails.relatedPlaylists.uploads;
+      getTopVideos(uploadsPlaylistId);
+    })
+    .catch((error) => console.log(error));
+}
+
+function getTopVideos(playlistId) {
+  fetch(
+    `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${playlistId}&maxResults=50&key=${API_KEY}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      // Fetching details for each video to sort by view count
+      const videoIds = data.items
+        .map((item) => item.snippet.resourceId.videoId)
+        .join(",");
+      fetchVideoDetailsAndSort(videoIds);
+    })
+    .catch((error) => console.log(error));
+}
+
+function fetchVideoDetailsAndSort(videoIds) {
+  fetch(
+    `https://www.googleapis.com/youtube/v3/videos?part=snippet,contentDetails,statistics&id=${videoIds}&key=${API_KEY}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      // Sorting videos by view count
+      const sortedVideos = data.items.sort(
+        (a, b) => b.statistics.viewCount - a.statistics.viewCount
+      );
+      displayVideos(sortedVideos.slice(0, 10)); // Displaying top 10 videos
+    })
+    .catch((error) => console.log(error));
 }
 
 function displayVideos(videos) {
