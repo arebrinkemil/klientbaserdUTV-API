a3bdabe41e2c4e38d51782fddf510754389c7a59
arebrinkemil <60137005+arebrinkemil@users.noreply.github.com>
2023-11-27 14:46:02 +0100

    Revert "Update script.js"
    This reverts commit 5007ef2e17af2da60af78e676c78469c6bd8884a.

--- script.js
+++ script.js
@@ -1,84 +1,82 @@
 const API_KEY = "AIzaSyBGoaAhnmGXO2mLUevU1VWGo8FGFiTjabQ "; //youtube
 
 let apiKey = "7d0aa209496930d33c0dff82ce1ef7ab"; //lastfm
-let limit = 128;
-let LastFmUrl = `https://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&api_key=${apiKey}&limit=${limit}&format=json`;
+let LastFmUrl = `https://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&api_key=${apiKey}&format=json`;
 
 fetch(LastFmUrl)
   .then((response) => response.json())
   .then((data) => {
     console.log("Top Artists:", data.artists.artist);
-    processArtists(data.artists.artist);
+    artistDropDown(data.artists.artist);
   })
   .catch((error) => console.error("Error:", error));
 
-// ... Existing code for fetching top artists ...
-
-async function processArtists(artists) {
-  let artistVideos = [];
-
-  for (let artist of artists) {
-    let videos = await searchMusicVideos(artist.name + " music video");
-    let topVideos = await getTopThreeVideos(videos);
-    artistVideos.push({ artist: artist.name, videos: topVideos });
+function artistDropDown(artists) {
+  let artistDropDown = document.getElementById("artistDropDown");
+  for (let i = 0; i < artists.length; i++) {
+    let option = document.createElement("option");
+    option.value = artists[i].name;
+    option.innerHTML = artists[i].name;
+    artistDropDown.appendChild(option);
   }
-
-  return artistVideos;
 }
 
-async function searchMusicVideos(query) {
-  let response = await fetch(
+document
+  .getElementById("artistDropDown")
+  .addEventListener("change", function () {
+    var selectedValue = this.value;
+    console.log(selectedValue);
+    searchMusicVideos(selectedValue + " music video");
+  });
+
+function searchMusicVideos(query) {
+  fetch(
     `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${encodeURIComponent(
       query
     )}&type=video&maxResults=10&key=${API_KEY}`
-  );
-  let data = await response.json();
-  return data.items.map((item) => item.id.videoId).join(",");
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      const videoIds = data.items.map((item) => item.id.videoId).join(",");
+      fetchVideoDetails(videoIds);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
 
-async function getTopThreeVideos(videoIds) {
-  let response = await fetch(
+function fetchVideoDetails(videoIds) {
+  fetch(
     `https://www.googleapis.com/youtube/v3/videos?part=snippet,contentDetails,statistics&id=${videoIds}&key=${API_KEY}`
-  );
-  let data = await response.json();
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      const sortedVideos = data.items.sort(
+        (a, b) => b.statistics.viewCount - a.statistics.viewCount
+      );
+      const slicedArray = sortedVideos.slice(0, 3);
 
-  let sortedVideos = data.items.sort(
-    (a, b) => b.statistics.viewCount - a.statistics.viewCount
-  );
-
-  return sortedVideos.slice(0, 3);
+      displayVideos(slicedArray);
+    })
+    .catch((error) => console.log(error));
 }
 
-// ... Code for fetching top artists ...
+function displayVideos(videos) {
+  const container = document.getElementById("video-container");
+  container.innerHTML = ""; // Clear previous results
 
-fetch(LastFmUrl)
-  .then((response) => response.json())
-  .then((data) => {
-    console.log("Top Artists:", data.artists.artist);
-    return processArtists(data.artists.artist);
-  })
-  .then((artistVideos) => {
-    console.log("Artist Videos:", artistVideos);
-    // Now artistVideos contains each artist with their top 3 videos
-  })
-  .catch((error) => console.error("Error:", error));
+  videos.forEach((video) => {
+    console.log(video);
+    const videoId = video.id;
+    const iframe = document.createElement("iframe");
+    iframe.src = `https://www.youtube.com/embed/${videoId}`;
+    iframe.width = "560";
+    iframe.height = "315";
+    iframe.frameBorder = "0";
+    iframe.allow =
+      "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
+    iframe.allowFullscreen = true;
 
-// function displayVideos(videos) {
-//   const container = document.getElementById("video-container");
-//   container.innerHTML = ""; // Clear previous results
-
-//   videos.forEach((video) => {
-//     console.log(video);
-//     const videoId = video.id;
-//     const iframe = document.createElement("iframe");
-//     iframe.src = `https://www.youtube.com/embed/${videoId}`;
-//     iframe.width = "560";
-//     iframe.height = "315";
-//     iframe.frameBorder = "0";
-//     iframe.allow =
-//       "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
-//     iframe.allowFullscreen = true;
-
-//     container.appendChild(iframe);
-//   });
-// }
+    container.appendChild(iframe);
+  });
+}
