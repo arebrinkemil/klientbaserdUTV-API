b059b64c5a0175454805d442830d4c677f48da5e
arebrinkemil <60137005+arebrinkemil@users.noreply.github.com>
2023-11-27 14:01:35 +0100

    simpler but better!

--- script.js
+++ script.js
@@ -26,83 +26,31 @@ document
   .addEventListener("change", function () {
     var selectedValue = this.value;
     console.log(selectedValue);
-    searchChannel(selectedValue);
+    searchMusicVideos(selectedValue + " music video");
   });
 
-function searchChannel(channelName) {
-  //var channelName = document.getElementById("query").value;
-
+function searchMusicVideos(query) {
   fetch(
-    `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${channelName}&type=channel&key=${API_KEY}`
+    `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${encodeURIComponent(
+      query
+    )}&type=video&maxResults=10&key=${API_KEY}`
   )
     .then((response) => response.json())
     .then((data) => {
-      if (data.items.length > 0) {
-        const channelId = data.items[0].id.channelId;
-        getChannelUploadsPlaylistId(channelId);
-      } else {
-        console.log("Channel not found");
-      }
+      displayVideos(data.items);
     })
     .catch((error) => {
       console.log(error);
     });
 }
 
-function getChannelUploadsPlaylistId(channelId) {
-  fetch(
-    `https://www.googleapis.com/youtube/v3/channels?part=contentDetails&id=${channelId}&key=${API_KEY}`
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      const uploadsPlaylistId =
-        data.items[0].contentDetails.relatedPlaylists.uploads;
-      getTopVideos(uploadsPlaylistId);
-    })
-    .catch((error) => console.log(error));
-}
-
-function getTopVideos(playlistId) {
-  fetch(
-    `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${playlistId}&maxResults=50&key=${API_KEY}`
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      // Fetching details for each video to sort by view count
-      const videoIds = data.items
-        .map((item) => item.snippet.resourceId.videoId)
-        .join(",");
-      fetchVideoDetailsAndSort(videoIds);
-    })
-    .catch((error) => console.log(error));
-}
-
-function fetchVideoDetailsAndSort(videoIds) {
-  fetch(
-    `https://www.googleapis.com/youtube/v3/videos?part=snippet,contentDetails,statistics&id=${videoIds}&key=${API_KEY}`
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      // Sorting videos by view count
-      const sortedVideos = data.items.sort(
-        (a, b) => b.statistics.viewCount - a.statistics.viewCount
-      );
-      displayVideos(sortedVideos.slice(0, 12)); // Displaying top 10 videos
-    })
-    .catch((error) => console.log(error));
-}
-
 function displayVideos(videos) {
   const container = document.getElementById("video-container");
   container.innerHTML = ""; // Clear previous results
 
-  const firstVideo = videos[0];
-  let backgroundImage = firstVideo.snippet.thumbnails.maxres.url;
-
-  document.body.style.backgroundImage = `url(${backgroundImage})`;
   videos.forEach((video) => {
     console.log(video);
-    const videoId = video.id;
+    const videoId = video.id.videoId;
     const iframe = document.createElement("iframe");
     iframe.src = `https://www.youtube.com/embed/${videoId}`;
     iframe.width = "560";
